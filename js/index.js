ea35c1022c9348882926660b9d56185d57a528bc
Keinerballesteros <kballesterosm@ufpso.edu.co>
2023-11-15 10:31:24 -0500

    subiendo borrador

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let myName = document.getElementById("names");
+let title = document.getElementById("name");
+myName.onclick = function () {
+    estableceNombreUsuario();
+  };
+function estableceNombreUsuario() {
+    let miName = prompt("Por favor, ingresa tu nombre:");
+    localStorage.setItem("nombre", miName);
+    title.textContent = "Hola," + miName;
+  }
+  if (!localStorage.getItem("nombre")) {
+    estableceNombreUsuario();
+  } else {
+    let nombreAlmacenado = localStorage.getItem("nombre");
+    title.textContent = "Hola," + nombreAlmacenado;
+  }
+  function login(){
+    window.location.href = "/subpaginas/register.html";
+  }
+  
+  
---
+++ js/register.js
@@ -0,0 +1,3 @@
+function window(){
+    window.location.href = "index.html";
+}
